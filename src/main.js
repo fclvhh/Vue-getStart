22421db50c44d0bf7ded15619899e0a141f698ca
fcdada <374822251@qq.com>
2022-06-12 01:46:09 +0800

    init vue-getStart
    安装了element + axios , 增加了 element插件，配置了vue-config，替换对的网址就能使用了

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './plugins/element.js'
+import axios from "axios"
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+console.log(axios);
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    https: 'false',
+    open: true,
+    proxy: {
+      "/admin":{
+        target:'http://ceshi5.dishait.cn/admin',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite:{
+          '^/admin': ""
+        }
+      }
+    }
+  }
 }
